1faa49a907e1ef9c51c9c2f5ac11c7e8bbbb9366
gayatrijhareal <gayatrixreal@gmail.com>
2025-09-03 15:20:52 +0530

    fifth commit

--- main.py
+++ main.py
@@ -7,7 +7,30 @@ print("hello how are you ")
 # hello()
 
 # // parameters and arguments
-
+#   // positional args
 def sum(a,b):
     print(f"the sum of the number : {a+b}")
 sum(12,12)
+
+# //default args
+
+def hello(name,age):
+    print(f"your name is {name} and your age is {age}")
+
+hello("Gayatri" , 18 )
+hello(age= 18  , name="Gayatri")
+
+def pallindrome(st):
+    rev =""
+    for  i in range(len(st)-1,-1,-1):
+        rev = rev + st[i]
+
+    if rev == st:
+        print("pallindrome")
+
+    else:
+        print("not an palindrome ")
+
+pallindrome("naman")
+pallindrome("cursor")
+
