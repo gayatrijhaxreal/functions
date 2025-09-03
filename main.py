8be7135d38a57e7a775e1ebf60f533c0cffe1786
gayatrijhareal <gayatrixreal@gmail.com>
2025-09-03 13:10:36 +0530

    third commit

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ print("Hello, World!")
 
 print("hello how are you ")
 
-def hello():
-    print("this is a hello fnc so i am doing hello")
-hello()
+# def hello():
+#     print("this is a hello fnc so i am doing hello")
+# hello()
+
+# // parameters and arguments
+
