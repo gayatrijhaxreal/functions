e6d2bd8c9b0f079749453c5ebe12df73f3073f6b
gayatrijhareal <gayatrixreal@gmail.com>
2025-09-03 15:38:02 +0530

    sixth  commit

--- main.py
+++ main.py
@@ -1,36 +1,36 @@
-print("Hello, World!")
+# print("Hello, World!")
 
-print("hello how are you ")
+# print("hello how are you ")
 
-# def hello():
-#     print("this is a hello fnc so i am doing hello")
-# hello()
+# # def hello():
+# #     print("this is a hello fnc so i am doing hello")
+# # hello()
 
-# // parameters and arguments
-#   // positional args
-def sum(a,b):
-    print(f"the sum of the number : {a+b}")
-sum(12,12)
+# # // parameters and arguments
+# #   // positional args
+# def sum(a,b):
+#     print(f"the sum of the number : {a+b}")
+# sum(12,12)
 
-# //default args
+# # //default args
 
-def hello(name,age):
-    print(f"your name is {name} and your age is {age}")
+# def hello(name,age):
+#     print(f"your name is {name} and your age is {age}")
 
-hello("Gayatri" , 18 )
-hello(age= 18  , name="Gayatri")
+# hello("Gayatri" , 18 )
+# hello(age= 18  , name="Gayatri")
 
-def pallindrome(st):
-    rev =""
-    for  i in range(len(st)-1,-1,-1):
-        rev = rev + st[i]
+# def pallindrome(st):
+#     rev =""
+#     for  i in range(len(st)-1,-1,-1):
+#         rev = rev + st[i]
 
-    if rev == st:
-        print("pallindrome")
+#     if rev == st:
+#         print(f"{st} is a pallindrome")
 
-    else:
-        print("not an palindrome ")
+#     else:
+#         print(f"{st} is not an palindrome ")
 
-pallindrome("naman")
-pallindrome("cursor")
+# pallindrome("naman")
+# pallindrome("cursor")
 
