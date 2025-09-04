63f36696d23f80a3cb08fc0531c33fb909683817
gayatrijhareal <gayatrixreal@gmail.com>
2025-09-04 11:07:20 +0530

    11th

--- questions/ques.py
+++ questions/ques.py
@@ -1,1 +1,16 @@
 """Questions of list"""
+
+"""Print positive and negative elements of an List ."""
+
+l = [-45, 67, 12, -68, -69,34]
+
+print("positive elements are :")
+for i in l:
+    if i >=0:
+        print(i)
+
+print("negative element are :")
+
+for i in l :
+    if i < 0:
+        print(i)
